f85f386b2d4108df002b66dd6578edf988cb8c64
zuaki <zuaki.a@gmail.com>
2019-03-23 13:27:15 +0900

    install vuetify

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "max-line-length": [true, 180]
   },
   parserOptions: {
     parser: "@typescript-eslint/parser"
